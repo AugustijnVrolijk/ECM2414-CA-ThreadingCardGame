437a1df19af57691a1d4c2f81b56fa834e0856af
isabellewalford <issywalf@gmail.com>
2022-11-04 15:58:50 +0000

    removed main and added to card

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,11 @@
 public class Card {
+
+    private int id;
+    private int cardId;
+    private int cardNumber;
+
+    public Card(int cardNumber) {
+        this.cardId = id++;
+        this.cardNumber = cardNumber;
+    }
 }
