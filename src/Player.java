da3ea4970d51242f31e596bd3165a29e0b4cbcaa
augustijn <av407@exeter.ac.uk>
2022-11-08 15:56:47 +0000

    player output file added

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,20 @@
 import java.util.ArrayList;
-
+import java.io.*;
 public class Player {
     private int playerId;
     private ArrayList<Card> cards = new ArrayList<>();
+    private File outputFile;
 
     Player(int playerId) {
         this.playerId = playerId;
+        this.outputFile = new File("../player"+String.valueOf(this.playerId)+"_output.txt");
     }
 
     public void addCard(Card card){
-        this.cards.add(card);
+        cards.add(card);
     }
     public Card removeCard(Card card) {
-        this.cards.remove(card);
+        cards.remove(card);
         return card;
     }
     public boolean checkHand() {
