f9b3f20cc14b2aad684eb9e98223a79b72ad32f4
isabellewalford <issywalf@gmail.com>
2022-11-08 14:37:11 +0000

    test classes added

---
+++ src/testing/testCard.java
@@ -0,0 +1,11 @@
+package testing;
+
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+
+public class testCard {
+}
