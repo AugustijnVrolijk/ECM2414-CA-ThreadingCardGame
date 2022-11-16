7cd08ee6221cd43c1efc31547481e2da5f4c7945
isabellewalford <issywalf@gmail.com>
2022-11-16 14:14:48 +0000

    methods checking and reading pack changed in CardGame.java

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -22,25 +23,58 @@ public class CardGame {
 
     public void startGame() throws IOException {
         Scanner userInput = new Scanner(System.in);  // Create a Scanner object
+        int numPlayers = 0;
+
+        System.out.println("Please enter the number of players:");
+        try {
+            numPlayers = userInput.nextInt();  // Read user input
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input for players");
+        }
+
         System.out.println("Please enter location of pack to load:");
         String fileName = userInput.nextLine(); // Read user input
-        if(checkPack(fileName)){
+        // todo - why tf does this not work with a string but is fine with a fucking int
+        if(checkPack(numPlayers, fileName)){ // check if pack is valid before reading
             readPack(fileName);
         } else {
             System.out.println("Pack Invalid");
         }
 
-        System.out.println("Please enter the number of players:");
-        try {
-            int players = userInput.nextInt();  // Read user input
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input for players");
-        }
+
 
     }
 
-    public boolean checkPack(String fileName) {
+    public boolean checkPack(int numPlayers, String fileName) throws IOException {
+
         boolean valid = true;
+        int lineNum = 0;
+        int cardNum = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                lineNum ++; // count lines
+                try {
+                    cardNum = Integer.parseInt(line);
+                } catch (NumberFormatException e) {
+                    // error will happen if the line does not contain a number
+                    valid = false;
+                }
+
+                if (cardNum == 0) {
+                    valid = false;
+                }
+            }
+            if (lineNum < (8 * numPlayers)){
+                // checks that there are enough cards in the pack
+                valid = false;
+            }
+        } catch (FileNotFoundException e) {
+            valid = false;
+        }
 
 
         return valid;
@@ -51,18 +85,14 @@ public class CardGame {
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         String line = br.readLine();
 
-
         while ((line = br.readLine()) != null) {
-            int cardNum = Integer.parseInt(line);
+            int cardNum = Integer.parseInt(line); // get card num from file
             int id = cards.size();
-            Card card = new Card(cardNum,id);
-            cards.put(id, card);
+            Card card = new Card(cardNum,id); // creates card object
+            cards.put(id, card); // adds to hashmap cards in CardGame
         }
     }
 
-    public void setUpGame(int NumPlayers) {
-
-    }
 
 
 
