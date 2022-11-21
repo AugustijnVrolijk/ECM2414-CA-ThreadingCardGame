897bbbe52a3618a7da00463a6d999727773243cc
augustijn <av407@exeter.ac.uk>
2022-11-21 14:25:15 +0000

    player output file added

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,12 +1,17 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class CardDeck {
 
     private int deckId;
+    private File outputFile;
     private ArrayList<Card> cardList = new ArrayList<>();
 
     CardDeck(int deckId){
         this.deckId = deckId;
+        String basePath = new File("").getAbsolutePath();
+        boolean temp = new File("outputTextFiles").mkdir();
+        outputFile = new File(String.format("%s/outputTextFiles/deck%d_output.txt",basePath, this.deckId));
     }
 
     public synchronized void addCard(Card card) {
@@ -22,5 +27,13 @@ public class CardDeck {
     }
     public synchronized int getDeckId() {
         return deckId;
+    }
+
+    public void recordFinalHand() {
+        try (FileWriter f = new FileWriter(outputFile, false);
+             BufferedWriter b = new BufferedWriter(f);
+             PrintWriter p = new PrintWriter(b)) {
+            p.println(String.format("Deck %d contents: %d %d %d %d",cardList.get(0).getCardNumber(), cardList.get(1).getCardNumber(), cardList.get(2).getCardNumber(), cardList.get(3).getCardNumber()));}
+        catch (IOException i) { i.printStackTrace(); }
     }
 }
