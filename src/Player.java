5f1b50af87582b99b507dc2af75fa6a27d66b119
augustijn <av407@exeter.ac.uk>
2022-11-22 15:52:13 +0000

    fixed cardgame topology

--- src/Player.java
+++ src/Player.java
@@ -23,8 +23,8 @@ public class Player extends Thread {
         this.playerId = playerId;
         this.preferredCard = playerId;
         String basePath = (new File("")).getAbsolutePath();
-        outputFile = new File(String.format("%s/outputTextFiles/player%d_output.txt", basePath, playerId));
-        appendToOutputFile(String.format("Player %d enters the game", playerId), false);
+        outputFile = new File(String.format("%s/outputTextFiles/player%d_output.txt", basePath, playerId)); //create output file to output folder
+        appendToOutputFile(String.format("Player %d enters the game", playerId), false); //finish setup in text file declaring player is created
     }
 
     public static void setNumberOfPlayers(int numberOfPlayers) {
@@ -32,17 +32,13 @@ public class Player extends Thread {
     }
 
     public void appendInitialHand(){
-        appendToOutputFile(String.format("player %d initial hand %d %d %d %d",playerId,
-                cards.get(0).getCardNumber(),
-                cards.get(1).getCardNumber(),
-                cards.get(2).getCardNumber(),
-                cards.get(3).getCardNumber()), true);
+        appendToOutputFile(String.format("player %d initial hand %s",playerId, cards.toString()), true);
     }
 
     private void appendToOutputFile(String text, boolean append) {
         try (FileWriter f = new FileWriter(outputFile, append);
              BufferedWriter b = new BufferedWriter(f);
-             PrintWriter p = new PrintWriter(b);) {
+             PrintWriter p = new PrintWriter(b);) { //using .io library (FileWriter) to write to text file, bufferedWriter and PrintWriter needed to append to file
             p.println(text); }
         catch (IOException i) { i.printStackTrace(); }
     }
@@ -57,13 +53,13 @@ public class Player extends Thread {
 
     public synchronized Card drawCard() {
         for(int i = 0; i < deckBefore.getCardList().size(); i++) {
-            if (deckBefore.getCardList().get(i).getCardNumber() == preferredCard) {
+            if (deckBefore.getCardList().get(i).getCardNumber() == preferredCard) { //iterate through cards in deck before, check if any of those cards are the preferred card
                 cards.add(deckBefore.getCardList().get(i));
                 appendToOutputFile(String.format("player %d draws a %d from deck %d",playerId, deckBefore.getCardList().get(i).getCardNumber(), deckBefore.getDeckId()), true);
-                return deckBefore.getCardList().get(i);
+                return deckBefore.getCardList().get(i); //return the card object, so it can be removed from the deck before - to avoid duplicates
             }
         }
-        int i = rand.nextInt(deckBefore.getCardList().size());
+        int i = rand.nextInt(deckBefore.getCardList().size()); //choose random card as none meet the requirements for this player
         cards.add(deckBefore.getCardList().get(i));
         appendToOutputFile(String.format("player %d draws a %d from deck %d",playerId, deckBefore.getCardList().get(i).getCardNumber(), deckBefore.getDeckId()), true);
         return deckBefore.getCardList().get(i);
@@ -72,19 +68,19 @@ public class Player extends Thread {
     public synchronized Card discardCard() {
         ArrayList<Card> temp = new ArrayList<>();
         for(int i = 0; i < cards.size(); i++) {
-            if (cards.get(i).getCardNumber() != preferredCard) {
+            if (cards.get(i).getCardNumber() != preferredCard) { //check which cards which this player holds don't meet the requirements
                 temp.add(cards.get(i));
             }
         }
-        int i = rand.nextInt(temp.size());
+        int i = rand.nextInt(temp.size()); //choose random card to discard from ones which don't meet the requirements to avoid the game stagnating
         cards.remove(temp.get(i));
         appendToOutputFile(String.format("player %d discards a %d to deck %d",playerId, temp.get(i).getCardNumber(), deckAfter.getDeckId()), true);
-        return temp.get(i);
+        return temp.get(i); //return the discarded cards, so it can be added to the deck after
     }
 
     public boolean checkHand() {
         for (int i = 0; i < cards.size()-1; i++) {
-            if (cards.get(i).getCardNumber() != cards.get(i+1).getCardNumber()){
+            if (cards.get(i).getCardNumber() != cards.get(i+1).getCardNumber()){ //check if any of the cards in a player's hand are different to each other
                 return false;
             }
         }
@@ -100,14 +96,14 @@ public class Player extends Thread {
     }
 
     public void stopPlayers(){
-        playing = false;
-        winningThread = playerId;
-        lock.notifyAll();
+        playing = false; //turns playing false so main loop in run stops
+        winningThread = playerId; //sets this player as the winning player to inform others
+        lock.notifyAll(); //removes lock in case other threads are waiting for others to finish
     }
 
     private synchronized void incrementIsReady() {
         isReady += 1;
-    }
+    } // synchronised so it isReady count stays accurate
 
     private synchronized void resetIsReady() {
         isReady = 0;
@@ -119,7 +115,7 @@ public class Player extends Thread {
                 if (checkHand()){
                     System.out.println(String.format("Player %d wins", playerId));
                     stopPlayers();
-                    appendToOutputFile(String.format("player %d exits",this.playerId), true);
+                    appendToOutputFile(String.format("player %d exits", playerId), true);
                     appendToOutputFile(String.format("Player %d final hand: %s", playerId, cards.toString()), true);
                     deckAfter.recordFinalHand();
                     stop();
@@ -133,11 +129,11 @@ public class Player extends Thread {
                 if(hasPlayed) {
                     incrementIsReady();
                     hasPlayed = false;
-                    if (isReady < numberOfPlayers) { //number of players
+                    if (isReady < numberOfPlayers) { //check if all other players have finished their turn, wait if they haven't
                         try {lock.wait();}
                         catch (InterruptedException ignored){System.out.println("Interrupted Exception caught");}
                     } else {
-                        lock.notifyAll();
+                        lock.notifyAll(); //every player has now finished their turn, they can continue to their next turn
                         resetIsReady();
                     }
                 }
